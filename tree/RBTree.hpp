22ab6a6cb239076351856b466fad54391d2be479
jzh <zhj199305@gmail.com>
2019-12-30 21:38:22 -0500

    red black tree

--- tree/RBTree.hpp
+++ tree/RBTree.hpp
@@ -11,6 +11,37 @@ class RBTree{
     public:
     
 
-}
+    private:
+    struct Node{
+        T element;
+        Node* left;
+        Node* right;
+        Node* parent;
+        bool color;
 
-#endif 
+        Node(const T& ele, Node* lt, Node* rt, bool clr):
+        element{ele}, left{lt}, right{rt}, color{clr} {}
+
+        Node(const T&& ele, Node* lt, Node* rt, bool clr): 
+        element{std::move(ele)}, left{lt}, right{rt}, color{clr} {} 
+    };
+
+    Node* root;
+
+    void insert(const T& ele, Node*& t){ }
+
+    void delete(const T& ele, Node*& t){ }
+
+    void left_rotate(Node*& t){
+        Node* y = t->right;
+        t->right = y->left;
+        y->left = x;
+        
+
+    }
+    
+
+    
+};
+
+#endif
