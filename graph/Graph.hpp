f2c568dd2a20b1cd39b671fb86d38abd20ab1f36
jzh <zhj199305@gmail.com>
2020-01-17 14:51:41 -0500

    graph declaration

--- graph/Graph.hpp
+++ graph/Graph.hpp
@@ -4,31 +4,17 @@
 #include <vector>
 #include <string>
 
+
+
 class Graph{
     public:
-        explicit Graph(std::vector<std::vector<int>> M);
-        void topsort();
+        explicit Graph(int Vnum);
+        int V;
+        int E;
+        std::vector<int> Adjs;
+        
+        void add_edge(int v, int w);
 
-    private:
-        struct Vertex{
-            int key;
-            int indegree;
-            int topnum;
-            std::vector<Vertex> adjs;
-
-            explicit Vertex(int num)
-            : key{num}, indegree{0}, topnum{0}{};
-
-            Vertex(const Vertex& x)
-            : key{x.key}, indegree{x.indegree}, topnum{x.topnum}{};
-
-            Vertex(const Vertex&& x)
-            : key{std::move(x.key)}, indegree{std::move(x.indegree)}, topnum{std::move(x.topnum)}
-            {};
-            
-        };
-
-        std::vector<Vertex> vertices;
 
         
 };
