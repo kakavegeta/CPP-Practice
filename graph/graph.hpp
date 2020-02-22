be9ada929dc71bfd2f89bd0d34bee67db3f0df3b
vegeka <zhj199305@gmail.com>
2020-02-22 02:16:16 -0500

    graph implementation

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -10,11 +10,54 @@ public:
     int V; //the number of vertices
     int E; //the number of edges
 
-    enum State{WHITE, GRAY, BLACK};
+    Graph(std::vector<T>& values)
+    : V{values.size()}, vertices(V)
+    {
+        for(int i=0; i<V; ++i){
+            vertices[i].data = values[i];
+            vertices[i].state = WHITE;
+            vertices[i].list = nullptr;
+        }
+    }
+
+    void set_edge(T x, T y){
+        for(int i=0; i<V; ++i){
+            if(vertices[i].data == x){
+                for (int j=0; j<V && j!=i; ++j){
+                    if(vertices[j]==y){
+                        insert_at_end(vertices[i].list, vertices[j]);
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    void print() const{
+        Node* node;
+        for(int i=0; i<V; ++i){
+            std::cout << "Vertex: " << vertices[i].data << " "
+            std::cout << "Connections: ";
+            node = vertices[i].list;
+            while(node != nullptr){
+                std::cout << node->vertex_prt->data << " ";
+                node = node->next;
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void bfs(T x);
+
+
+private:
+    
     struct Vertex;
+    enum State{WHITE, GRAY, BLACK};
 
     struct Node{
-        Vertex* vertex;
+        Vertex* vertex_ptr;
         Node* next;
     }
 
@@ -26,11 +69,35 @@ public:
 
     std::vector<Vertex> vertices;
 
-public:
-    Graph() = default;
-    Graph(std::vector<T>&);
-    void addEdge(T, T);
-    void printGraph();
-    ~Graph();
+    Node* get_node(Vertex* v){
+        Node* node = new Node;
+        node->vertex_prt = v;
+        node->next = nullptr;
+        return node;
+    }
+
+    void insert_at_end(Node*& node, Vertex* v){
+        Node* new_node = get_node(v);
+        if(node==nullptr){
+            node = new_node;
+        }else {
+            Node* curr = node;
+            while(curr != nullptr){
+                curr = curr->next;
+            }
+            curr->next  new_node;
+        }
+    }
+
+    void delete_all_after(Node*& node){
+        Node* curr = node;
+        Node* next_node;
+        while(curr != nullptr){
+            next_node = curr->next;
+            delete(curr);
+            curr = next_node;
+        }
+    }
+
 
 }
