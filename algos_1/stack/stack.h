d0e3f95fe617a065cb7141bf881add9f2a17c667
vegeka <zhj199305@gmail.com>
2020-05-23 05:29:55 -0400

    renew

---
+++ algos_1/stack/stack.h
@@ -0,0 +1,56 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <exception>
+#include <cstdint>
+#include <iostream>
+
+const int DEFAULT_CAPACITY = 100;
+
+template<typename T>
+class Stack {
+    public:
+    Stack(int capacity = DEFAULT_CAPACITY)
+        :_cap{capacity}, _size{0}, _elements{new T[_cap]} {} 
+    
+    Stack(const Stack& stk):
+        :_cap{stk._cap}, _size{stk._size}, _elements{new T[_cap]} {
+        for(int i=0; i<size; ++i){
+            _elements[i] = stk._elements[i];
+        }
+    }
+
+    Stack & operator = (const Stack& stk);
+
+    int size() {
+        return _size;
+    }
+
+    bool empty() {
+        return _size == 0;
+    }
+
+    void push(const T& t) {
+        if(_size == _cap) return; 
+        _elements[_size++] = t;
+    }
+
+    void pop() {
+        if(empty()) return;
+        _size--;
+    }
+
+    int top() {
+        if (empty()) return;
+        return _elements[_size-1];
+    }
+
+    private:
+    int _cap;
+    int _size;
+    T * _elements;
+}
+
+
+
+#endif
