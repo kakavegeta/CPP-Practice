cec0492ed5139c2e790108a7348feb9b2aa96ef5
vegeka <zhj199305@gmail.com>
2020-05-23 13:38:23 -0400

    stack

--- algo_1/include/stack.h
+++ algo_1/include/stack.h
@@ -13,12 +13,15 @@ class Stack {
     Stack(int capacity = DEFAULT_CAPACITY)
         :_cap{capacity}, _size{0}, _elements{new T[_cap]} {} 
     
-    Stack(const Stack& stk):
+    Stack(const Stack& stk)
         :_cap{stk._cap}, _size{stk._size}, _elements{new T[_cap]} {
-        for(int i=0; i<size; ++i){
+
+        for(int i=0; i<size; ++i) {
             _elements[i] = stk._elements[i];
         }
     }
+
+    ~ Stack() {delete [] _elements;}
 
     Stack & operator = (const Stack& stk);
 
@@ -27,11 +30,11 @@ class Stack {
     }
 
     bool empty() {
-        return _size == 0;
+        return (_size == 0);
     }
 
     void push(const T& t) {
-        if(_size == _cap) return; 
+        if(_size == _cap) throw StackOutofBoundException{}; 
         _elements[_size++] = t;
     }
 
@@ -41,16 +44,17 @@ class Stack {
     }
 
     int top() {
-        if (empty()) return;
+        if (empty()) throw StackEmptyException {};
         return _elements[_size-1];
     }
 
     private:
     int _cap;
     int _size;
-    T * _elements;
-}
+    T* _elements;
 
-
+    class StackEmptyException {};
+    class StackOutofBoundException {}; 
+};
 
 #endif
